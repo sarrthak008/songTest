4355b05d2e941ee8812dcee5fb6d950663437cd5
sarrthak008 <sarthaknavale08@gmail.com>
2024-11-22 19:42:09 +0530

    add real like structure

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const Player = ({songurl}) => {
   return (
-    <div className='fixed bottom-0 left-0 w-full'>
-      <AudioPlayer src={songurl} width='100%' volume={20} autoPlay color='#86EFAC' sliderColor='#86EFAC' backgroundColor='#1F2937'/>
+    <div className='fixed -bottom-2 left-0 w-full'>
+      <AudioPlayer src={songurl} width='100%' volume={20} autoPlay color='#86EFAC' sliderColor='#86EFAC' backgroundColor='transparent'/>
     </div>
   )
 }
--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -13,10 +13,15 @@ const SongCard = ({ songdetails, SongUrl}) => {
 
 
   return (
-    <div className='h-full w-full bg-gray-800 rounded-lg overflow-hidden section '>
+    <div className='h-full w-full bg-gray-800 rounded-lg overflow-hidden section relative' onClick={handlePlayPause}>
       <div className='h-[100%] w-full overflow-hidden relative'>
         <img src={poster} className='h-full w-full object-cover ' />
         <div className='cover absolute top-0 left-0 h-full w-full bg-gradient-to-t from-black  '></div>
+      </div>
+      <div className='h-full w-14  absolute top-0 right-0 bg-gradient-to-l from-black flex items-center justify-end flex-col gap-11 pb-[40%]'>
+          <i className="ri-heart-3-line text-4xl opacity-75"></i>
+          <i className="ri-chat-1-line text-4xl opacity-75"></i>
+          <i className="ri-share-line text-4xl opacity-75"></i>
       </div>
     </div>
   )
